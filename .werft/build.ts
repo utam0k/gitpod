da928ea3507f5ccbd4005147a44e9b789988548a
Mads Hartmann <mads379@gmail.com>
2021-12-17 09:35:31 +0000

    WIP: SSH fixes

--- .werft/build.ts
+++ .werft/build.ts
@@ -310,7 +310,7 @@ export async function build(context, version) {
 
         werft.log(vmSlices.KUBECONFIG, 'Copying k3s kubeconfig')
         exec(`sleep 10`)
-        exec(`ssh -i /workspace/.ssh/id_rsa_harvester_vm ubuntu@127.0.0.1 'sudo cat /etc/rancher/k3s/k3s.yaml' > k3s.yml`)
+        exec(`ssh -i /workspace/.ssh/id_rsa_harvester_vm ubuntu@127.0.0.1 -o StrictHostKeyChecking=no 'sudo cat /etc/rancher/k3s/k3s.yaml' > k3s.yml`)
         exec(`cat k3s.yml`)
 
         werft.done(phases.VM)
