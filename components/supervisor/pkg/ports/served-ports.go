515c5bd5f2fe90b3fe85df0dee1f04c3bfbb00f4
Jean Pierre <jeanp413@hotmail.com>
2022-01-11 03:11:56 +0000

    Don't try to connect to opened ports Otherwise it causes an exception to be thrown constantly in the vscode-js-debug extension while debugging causing the extension host to crash after some time

--- components/supervisor/pkg/ports/served-ports.go
+++ components/supervisor/pkg/ports/served-ports.go
@@ -6,9 +6,12 @@ package ports
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"sort"
 	"strconv"
@@ -20,7 +23,7 @@ import (
 
 // ServedPort describes a port served by a local service.
 type ServedPort struct {
-	Address          string
+	Address          net.IP
 	Port             uint32
 	BoundToLocalhost bool
 }
@@ -33,7 +36,9 @@ type ServedPortsObserver interface {
 	// the whole list is returned.
 	// When the observer stops operating (because the context as canceled or an irrecoverable
 	// error occurred), the observer will close both channels.
-	Observe(ctx context.Context) (<-chan []ServedPort, <-chan error)
+	// WorkspaceIP is the IP where there reverse proxy is bound to exposed local ports, entries
+	// with this IP will be ignored
+	Observe(ctx context.Context, workspaceIP string) (<-chan []ServedPort, <-chan error)
 }
 
 const (
@@ -51,7 +56,7 @@ type PollingServedPortsObserver struct {
 }
 
 // Observe starts observing the served ports until the context is canceled.
-func (p *PollingServedPortsObserver) Observe(ctx context.Context) (<-chan []ServedPort, <-chan error) {
+func (p *PollingServedPortsObserver) Observe(ctx context.Context, workspaceIP string) (<-chan []ServedPort, <-chan error) {
 	if p.fileOpener == nil {
 		p.fileOpener = func(fn string) (io.ReadCloser, error) {
 			return os.Open(fn)
@@ -94,7 +99,13 @@ func (p *PollingServedPortsObserver) Observe(ctx context.Context) (<-chan []Serv
 					continue
 				}
 				for _, port := range ps {
-					key := fmt.Sprintf("%s:%d", port.Address, port.Port)
+					if port.Address.String() == workspaceIP {
+						// Ignore entries that are bound to the workspace ip address
+						// as they are created by the reverse proxy
+						continue
+					}
+
+					key := fmt.Sprintf("%s:%d", hex.EncodeToString(port.Address), port.Port)
 					_, exists := visited[key]
 					if exists {
 						continue
@@ -144,17 +155,15 @@ func readNetTCPFile(fc io.Reader, listeningOnly bool) (ports []ServedPort, err e
 
 		ports = append(ports, ServedPort{
 			BoundToLocalhost: locallyBound,
-			Address:          addr,
+			Address:          hexDecode32bigNA([]byte(addr)),
 			Port:             uint32(port),
 		})
 
 		sort.Slice(ports, func(i, j int) bool {
-			sortedByAddress := ports[i].Address > ports[j].Address
-			if ports[i].Address == ports[j].Address {
-				sortedByPort := ports[i].Port < ports[j].Port
-				return sortedByPort
+			if ports[i].Address.Equal(ports[j].Address) {
+				return ports[i].Port < ports[j].Port
 			}
-			return sortedByAddress
+			return bytes.Compare(ports[i].Address, ports[j].Address) < 0
 		})
 
 		sort.Slice(ports, func(i, j int) bool {
@@ -166,4 +175,32 @@ func readNetTCPFile(fc io.Reader, listeningOnly bool) (ports []ServedPort, err e
 	}
 
 	return
+}
+
+// hexDecode32big decodes sequences of 32bit big endian bytes.
+func hexDecode32bigNA(src []byte) net.IP {
+	buf := make(net.IP, net.IPv6len)
+
+	blocks := len(src) / 8
+	for block := 0; block < blocks; block++ {
+		for i := 0; i < 4; i++ {
+			a := fromHexChar(src[block*8+i*2])
+			b := fromHexChar(src[block*8+i*2+1])
+			buf[block*4+3-i] = (a << 4) | b
+		}
+	}
+	return buf[:blocks*4]
+}
+
+// fromHexChar converts a hex character into its value.
+func fromHexChar(c byte) uint8 {
+	switch {
+	case '0' <= c && c <= '9':
+		return c - '0'
+	case 'a' <= c && c <= 'f':
+		return c - 'a' + 10
+	case 'A' <= c && c <= 'F':
+		return c - 'A' + 10
+	}
+	return 0
 }
